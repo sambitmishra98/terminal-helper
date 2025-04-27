162ed77c7f0d47ada0738e40b2082b11973c5056
sambit98-spitfire <sambit98@tamu.edu>
2025-04-27 16:22:23 -0500

    Rename directories.

--- terminal-paths-helper.sh
+++ terminal-paths-helper.sh
@@ -17,7 +17,7 @@ setup_paths()
     export VENVS="$SCRATCH/.venvs/"
     export WORKSPACES="$SCRATCH/.workspaces/"
     export SUPPLEMENTARY="$SCRATCH/supplementary/"
-    export MESHES="$SUPPLEMENTARY/gmsh_files/"
+    export MESHES="$SUPPLEMENTARY/gmsh-files/"
     export PARTITIONS="$SUPPLEMENTARY/pyfr-native-files/"
     export GITHUB="$SCRATCH/.github/"
     export SAMBITMISHRA98="$GITHUB/sambitmishra98/"
@@ -26,7 +26,19 @@ setup_paths()
     export SAMBITMISHRA98_PYFR_DEVELOP="$SAMBITMISHRA98_PYFR/develop/"
     export LOCAL="$SCRATCH/.local/"
     export DOWNLOADS="$LOCAL/downloads/"
-    export INSTALLS="$LOCAL/installation/"
+    export INSTALLS="$LOCAL/installs/"
+
+    for path in "$EFFORTS" \
+                "$VENVS" \
+                "$WORKSPACES" \
+                "$SUPPLEMENTARY" "$MESHES" "$PARTITIONS" \
+                "$GITHUB" "$SAMBITMISHRA98" \
+                "$PYFR_BRANCHES" "$SAMBITMISHRA98_PYFR" \
+                "$LOCAL" "$DOWNLOADS" "$INSTALLS"; do
+        if [ -d "${path}" ] ; then echo -e "\e[1;32m Exists: \e[0m ${path}"
+        else                       echo -e "\e[1;31m Doesn't exist: \e[0m ${path}"
+        fi
+    done
 }
 
 create_paths()
@@ -57,21 +69,21 @@ print_paths()
     echo -e "\e[1;32m--------------------\e[0m"
     # Display essential variables in a tree structure
     echo -e "\e[1;32mEssential variables:\e[0m"
-    echo -e "└─ SCRATCH:               $SCRATCH"
-    echo -e "   ├─ EFFORTS:             $EFFORTS"
-    echo -e "   ├─ VENVS:               $VENVS"
-    echo -e "   ├─ WORKSPACES:          $WORKSPACES"
-    echo -e "   ├─ SUPPLEMENTARY:       $SUPPLEMENTARY"
-    echo -e "   │   ├─ MESHES:           $MESHES"
-    echo -e "   │   └─ PARTITIONS:       $PARTITIONS"
-    echo -e "   ├─ GITHUB:              $GITHUB"
-    echo -e "   │   └─ SAMBITMISHRA98:   $SAMBITMISHRA98"
-    echo -e "   ├─ PYFR_BRANCHES:       $PYFR_BRANCHES"
+    echo -e "└─ SCRATCH:                            $SCRATCH"
+    echo -e "   ├─ EFFORTS:                         $EFFORTS"
+    echo -e "   ├─ VENVS:                           $VENVS"
+    echo -e "   ├─ WORKSPACES:                      $WORKSPACES"
+    echo -e "   ├─ SUPPLEMENTARY:                   $SUPPLEMENTARY"
+    echo -e "   │   ├─ MESHES:                      $MESHES"
+    echo -e "   │   └─ PARTITIONS:                  $PARTITIONS"
+    echo -e "   ├─ GITHUB:                          $GITHUB"
+    echo -e "   │   └─ SAMBITMISHRA98:              $SAMBITMISHRA98"
+    echo -e "   ├─ PYFR_BRANCHES:                   $PYFR_BRANCHES"
     echo -e "   │   ├─ SAMBITMISHRA98_PYFR:         $SAMBITMISHRA98_PYFR"
     echo -e "   │   └─ SAMBITMISHRA98_PYFR_DEVELOP: $SAMBITMISHRA98_PYFR_DEVELOP"
-    echo -e "   └─ LOCAL:               $LOCAL"
-    echo -e "       ├─ DOWNLOADS:         $DOWNLOADS"
-    echo -e "       └─ INSTALLS:          $INSTALLS"
+    echo -e "   └─ LOCAL:                           $LOCAL"
+    echo -e "       ├─ DOWNLOADS:                   $DOWNLOADS"
+    echo -e "       └─ INSTALLS:                    $INSTALLS"
 }
 
 print_libraries_versions()
