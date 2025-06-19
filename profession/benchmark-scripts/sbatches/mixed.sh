c87fc69b74479d172e5b633317a93cb2737ce59b
sambitmishra98 <sambit98@gmail.com>
2025-06-19 14:42:48 -0500

    update for lb

--- profession/benchmark-scripts/sbatches/mixed.sh
+++ profession/benchmark-scripts/sbatches/mixed.sh
@@ -2,9 +2,24 @@
 
 # Usage:  pyfr-select.sh <backend> <mesh> <cfg>
 
-if   [[ ${OMPI_COMM_WORLD_RANK} -eq  0 ]]; then CMD="pyfr run -Plb -bcuda   "
-else                                            CMD="pyfr run -Plb -bopenmp "
+if   [[ ${OMPI_COMM_WORLD_RANK} -eq  0 ]]
+then 
+export CUDA_VISIBLE_DEVICES=0
+CMD="pyfr run -Plb -bcuda "
+
+elif [[ ${OMPI_COMM_WORLD_RANK} -eq  12 ]]
+then 
+
+export CUDA_VISIBLE_DEVICES=1
+CMD="pyfr run -Plb -bcuda "
+
+else
+
+CMD="pyfr run -Plb -bopenmp "
 fi
+
+# Echo rank and CUDA
+echo "Rank ${OMPI_COMM_WORLD_RANK} using CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 
 echo $CMD $@
 eval $CMD $@
